f94fed5e6a3a413709dd01c32fc00aa4e85ea594
Leo McClellan <mcclellan2@usf.edu>
2024-10-26 12:27:00 -0400

    Add canvas setup and event handling logic.

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
-const canvas = document.getElementById("myCanvas");  // get canvas
+// Task 2: Configure the JavaScript for Drawing Context
+const canvas = document.querySelector("#myCanvas");  // get canvas
 const ctx = canvas.getContext('2d');  // get the 2d drawing context
 
+// Setup the canvas by drawing a background
+ctx.fillStyle = '#f0f0f0';
+ctx.fillRect(0,0, canvas.width, canvas.height);
 
+// Setup Opacity
+let opacity = 1;
+
+function drawCircle(x,y) {
+    ctx.fillStyle = `rgba(0, 255, 0, ${opacity})`;
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2, false);
+    ctx.fill();
+};
+
+canvas.addEventListener("click", (event) => {
+    drawCircle(event.offsetX, event.offsetY);
+});
+
+document.querySelector("#clearCanvas").addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+document.querySelector("#opacity").addEventListener("change", (event) => {
+    opacity = event.target.value;
+});
+
+// // drawing rectangle
+// ctx.strokeStyle = '#e74c3c';
+// ctx.lineWidth = 5;
+// ctx.strokeRect(0,0,500,500);
